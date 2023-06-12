f785e22815b18f67251778c23d6623e04586219a
Romainvilleneuve78 <romainvilleneuve78@gmail.com>
2023-06-12 15:04:53 +0200

    init

---
+++ src/Pages/Page1.jsx
@@ -0,0 +1,13 @@
+// src/pages/Page1.jsx
+import React from 'react';
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      <p>Ceci est le contenu de la page 1.</p>
+    </div>
+  );
+}
+
+export default Page1;
---
+++ src/Pages/Page2.jsx
@@ -0,0 +1,13 @@
+// src/pages/Page2.jsx
+import React from 'react';
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page 2</h1>
+      <p>Ceci est le contenu de la page 2.</p>
+    </div>
+  );
+}
+
+export default Page2;
