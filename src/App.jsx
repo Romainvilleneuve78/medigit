6e5895aeab5f5b654c9ef2bba85f519c6eb78271
Basile11 <basile.truquin@gmail.com>
2023-06-13 15:52:25 +0200

    AddOrdo

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import Page1 from './Pages/Accueil/Accueil';
 import Page2 from './Pages/Ordonnances/Ordonnances';
 import Page3 from './Pages/Rappels/Rappels';
 import Page4 from './Pages/Profil/Profil';
+import AddOrdo from './Pages/AddOrdo/AddOrdo';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -34,19 +35,21 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
 <Router>
 <nav>
   <Link to="/page1">Page 1</Link>
   <Link to="/page2">Page 2</Link>
   <Link to="/page3">Page 3</Link>
   <Link to="/page4">Page 4</Link>
+  <Link to="/addOrdo">Ajouter une ordonnance</Link>
 </nav>
 <Routes> {/* Remplacez le composant Route par Routes */}
   <Route path="/page1" element={<Page1 />} /> {/* Utilisez l'attribut element pour définir le composant */}
   <Route path="/page2" element={<Page2 />} />
   <Route path="/page3" element={<Page3 />} />
   <Route path="/page4" element={<Page4 />} />
+  <Route path="/addOrdo" element={<AddOrdo />} />
   
 </Routes>
 </Router>
