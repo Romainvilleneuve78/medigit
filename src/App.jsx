84ac110fe1f7fc1aec81e3933f3b0dbf13ac5185
Maxime-Thirion <thirionmaxime02@gmail.com>
2023-06-13 16:48:43 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,81 +2,61 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignIn_Page from './Pages/SignIn_Page'
+import SignUp_Page from './Pages/SignUp_Page'
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importez également Routes
-
 
 import Page1 from './Pages/Accueil/Accueil';
 import Page2 from './Pages/Ordonnances/Ordonnances';
 import Page3 from './Pages/Rappels/Rappels';
 import Page4 from './Pages/Profil/Profil';
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+      <header>
+        <nav class="navbar">
+            <a href=""><h1>MeDigit.</h1></a>
+            <h2>Recuperez vos ordonnaces n'importe où et nimporte quand</h2>
+            <a href="">
+              <div class="nav-links ">
+                  <ul>
+                      <li>User123</li>
+                      <li><img class="user" src="images/user.png" alt="Logo"></img></li>
+                  </ul>
+              </div>
+            </a>
+        </nav>
+      </header>
 
-
-<Router>
-<nav>
-  <Link to="/page1">Page 1</Link>
-  <Link to="/page2">Page 2</Link>
-  <Link to="/page3">Page 3</Link>
-  <Link to="/page4">Page 4</Link>
-
-</nav>
-<Routes> {/* Remplacez le composant Route par Routes */}
-  <Route path="/page1" element={<Page1 />} /> {/* Utilisez l'attribut element pour définir le composant */}
-  <Route path="/page2" element={<Page2 />} />
-  <Route path="/page3" element={<Page3 />} />
-  <Route path="/page4" element={<Page4 />} />
-  
-</Routes>
-</Router>
+      <nav class="menu">
+        <div class="links ">
+            <ul>
+              <li><Link to="/page1">Home</Link></li>
+              <li><Link to="/page2">Mes Ordonnances</Link></li>
+              <li><Link to="/page3">Mon Profil</Link></li>
+              <li><Link to="/page4">Mes Rappels</Link></li>
+            </ul>
+        </div>    
+      </nav>
+    
+      <Routes> {/* Remplacez le composant Route par Routes */}
+        <Route path="/page1" element={<Page1 />} /> {/* Utilisez l'attribut element pour définir le composant */}
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+        <Route path="/SignUp_Page" element={<SignUp_Page />} />
+        <Route path="/SignIn_Page" element={<SignIn_Page />} />
+      </Routes>
+      </Router>
     </>
   )
 }
 
 export default App
-// src/App.jsx
-// src/App.jsx
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <nav>
-//         <Link to="/page1">Page 1</Link>
-//         <Link to="/page2">Page 2</Link>
-//       </nav>
-//       <Routes> {/* Remplacez le composant Route par Routes */}
-//         <Route path="/page1" element={<Page1 />} /> {/* Utilisez l'attribut element pour définir le composant */}
-//         <Route path="/page2" element={<Page2 />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
--- src/Pages/Accueil/Accueil.jsx
+++ src/Pages/Accueil/Accueil.jsx
@@ -1,13 +1,38 @@
 // src/pages/Page1.jsx
 import React from 'react';
+import './Accueil.css'
 import SignIn from '../../components/SignIn';
 import SignUp from '../../components/SignUp';
 import SignIn_Page from '../SignIn_Page'
+
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importez également Routes
 import SignUp_Page from '../SignUp_Page';
 function Page1() {
+
+  const slides = [
+    {
+      id: 1,
+      imageUrl: '../../../images/banniere1.jpeg',
+      title: 'Première image',
+      description: 'Description de la première image',
+    },
+    {
+      id: 2,
+      imageUrl: '../../../images/banniere2.jpeg',
+      title: 'Deuxième image',
+      description: 'Description de la deuxième image',
+    },
+    // Ajoutez plus de diapositives selon vos besoins
+  ];
+
   return (
-    <div>
+    <>
+      <div class='Carousel'>
+        <div class='item'>
+          <div class='slide1'></div>
+        </div>
+      </div>
       <h1>Accueil</h1>
       <p>Mode d'emploi de l'utilisation du site web et comment obtenir une ordonnance</p>
 
@@ -21,7 +46,7 @@ function Page1() {
   <Route path="/SignUp_Page" element={<SignUp_Page />} />
   
 </Routes>
-    </div>
+    </>
   );
 }
 
