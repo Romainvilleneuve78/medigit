1c1ea895cd331009bddab64f96e9241c67a06843
bastien <bastien.pereira.20@efrei.net>
2023-06-23 11:16:34 +0200

    com

--- src/Pages/SignUp_Page.jsx
+++ src/Pages/SignUp_Page.jsx
@@ -1,11 +1,12 @@
 // src/pages/Page2.jsx
 import React from 'react';
 import SignUp from '../components/SignUp';
+import './Sign.css'
+
 function SignUp_Page() {
   return (
     <div>
-      <h1>Sign Up</h1>
-      <p>Enregistrez-vous</p>
+      <h1 class="Title">Sign Up</h1>
       <SignUp />
 
     </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,49 +6,111 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   const [fullName, setFullName] = useState('');
 
-//   const handleEmailChange = (event) => {
-//     setEmail(event.target.value);
-//   };
+  //   const handleEmailChange = (event) => {
+  //     setEmail(event.target.value);
+  //   };
 
-//   const handlePasswordChange = (event) => {
-//     setPassword(event.target.value);
-//   };
+  //   const handlePasswordChange = (event) => {
+  //     setPassword(event.target.value);
+  //   };
 
-//   const handleFullNameChange = (event) => {
-//     setFullName(event.target.value);
-//   };
+  //   const handleFullNameChange = (event) => {
+  //     setFullName(event.target.value);
+  //   };
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Appeler votre API backend pour gérer l'inscription avec les données email, password et fullName
-//     // ...
-//   };
-//   mettre dans le form : onSubmit={handleSubmit}
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     // Appeler votre API backend pour gérer l'inscription avec les données email, password et fullName
+  //     // ...
+  //   };
+  //   mettre dans le form : onSubmit={handleSubmit}
   return (
-    <div>
-      <form >
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-        //   onChange={handleEmailChange}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-        //   onChange={handlePasswordChange}
-        />
-        <label htmlFor="fullName">Full Name:</label>
-        <input
-          type="text"
-          id="fullName"
-          value={fullName}
-        //   onChange={handleFullNameChange}
-        />
-        <button type="submit">Sign Up</button>
+    <div class="log">
+      <form action="" method="post">
+        <div class="txt_field">
+
+          <input
+            type="texte"
+            placeholder='Entrez votre firstname '
+            id="firstname"
+            value={email}
+            required
+          //   onChange={handleEmailChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="secondname">Second Name:</label>
+          <input
+            type="text"
+            id="secondname"
+            value={password}
+          //   onChange={handlePasswordChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="sexe">Sexe:</label>
+          <input
+            type="text"
+            id="sexe"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="birthdate">Birth date:</label>
+          <input
+            type="date"
+            id="birthdate"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="phone">Phone :</label>
+          <input
+            type="tel"
+            id="phone"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="fix">Fix :</label>
+          <input
+            type="tel"
+            id="fix"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="email">Email :</label>
+          <input
+            type="email"
+            id="email"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="Refdoctor">médecin traitant :</label>
+          <input
+            type="text"
+            id="Refdoctor"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="txt_field">
+          <label htmlFor="secu">N° de sécurité sociale :</label>
+          <input
+            type="text"
+            id="Refdoctor"
+            value={fullName}
+          //   onChange={handleFullNameChange}
+          />
+        </div>
+        <div class="center-button"><button type="submit">Sign Up</button></div>
       </form>
     </div>
   );
