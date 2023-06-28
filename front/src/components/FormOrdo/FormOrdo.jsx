77e5346d3f168fd06eca7e1be59ce8983bb1ec35
Romainvilleneuve78 <romainvilleneuve78@gmail.com>
2023-06-28 13:45:05 +0200

    TETET

--- front/src/components/FormOrdo/FormOrdo.jsx
+++ front/src/components/FormOrdo/FormOrdo.jsx
@@ -11,9 +11,15 @@ class FormOrdo extends Component {
     };
   }
 
+  supprimerFormulaire = (index) => {
+    const nouveauxFormulaires = [...formulaires];
+    nouveauxFormulaires.splice(index, 1);
+    setFormulaires(nouveauxFormulaires);
+  };
+
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
-  }
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -21,30 +27,50 @@ class FormOrdo extends Component {
     console.log('Médicament:', this.state.medic);
     console.log('Dose:', this.state.dose);
     console.log('Description:', this.state.descr);
-    // Réinitialiser le formulaire
-    this.setState({ medic: '', dose: '', descr: '' });
-  }
+    // Ne pas réinitialiser le formulaire
+  };
 
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} class="cont">
-            <label className='in'>
-                <text style={{paddingBottom:'100px'}}>Médicament:</text>
-                <input type="text" name="medic" value={this.state.medic} onChange={this.handleChange} className='input'/>
-            </label>
-            <br />
-            <label className='in'>
-                Dose:
-                <input type="text" name="dose" value={this.state.dose} onChange={this.handleChange} className='input'/>
-            </label>
-            <br />
-            <label className='indes'>
-                <text>Description:</text>
-                <textarea type="text" name="descr" value={this.state.descr} onChange={this.handleChange} className='input' row="3"/>
-            </label>
-            <br />
-            
+        <form onSubmit={this.handleSubmit} className="cont">
+          <label className="in">
+            <text style={{ paddingBottom: '100px' }}>Médicament:</text>
+            <input
+              type="text"
+              name="medic"
+              value={this.state.medic}
+              onChange={this.handleChange}
+              className="input"
+            />
+          </label>
+          <br />
+          <label className="in">
+            Dose:
+            <input
+              type="text"
+              name="dose"
+              value={this.state.dose}
+              onChange={this.handleChange}
+              className="input"
+            />
+          </label>
+          <br />
+          <label className="indes">
+            <text>Description:</text>
+            <textarea
+              type="text"
+              name="descr"
+              value={this.state.descr}
+              onChange={this.handleChange}
+              className="input"
+              rows="3"
+            />
+          </label>
+          <br />
+          <button type="submit">Valider</button>
+          <button onClick={() => this.props.supprimerFormulaire(this.props.index)}>Supprimer</button>
+
         </form>
       </div>
     );
