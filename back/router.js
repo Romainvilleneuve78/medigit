92d687736d1a9d6597b750051851710c04548aef
Maxime-Thirion <thirionmaxime02@gmail.com>
2023-06-29 16:25:13 +0200

    aaa

--- back/router.js
+++ back/router.js
@@ -1,30 +1,41 @@
-import { Router } from 'express'
-const router = Router()
+const express = require("express");
+const router = express.Router();
+var user_model = require('./UserModel');
 
-//récupération des controllers
-import { createUser, loginUser, logoutUser, getUser } from './controllers/UserController.js'
 
-import { createCredit, answerCredit } from './controllers/transController.js'
+module.exports = router;
 
-import loginBank from './controllers/bankController.js'
-
-//redirect
-router.get('/', (req, res) => {
-    res.redirect('/index.html');
+router.get("/", (req, res) => {
+    res.json("API MAX V 0.1");
 });
 
-/*
-// pour le userController
-router.post('/register', createUser)
-router.post('/login_client', loginUser)
-router.post('/logout', logoutUser)
-router.post('/getprofile', getUser) // à implémenter pour faire une page profil, mais voir pour utiliser Vue js
 
-// pour le bankController
-router.post('/login_banque', loginBank)
 
-// pour le transController
-router.post('/credit', createCredit)
-router.post('/answer_credit', answerCredit)
-*/
-export default router
+router.get("/user/list", (req, res) => {
+    user_model.list_utilisateurs().then(result => {
+        console.log("Result received:", result);
+        res.send(result);
+    }).catch(err => {
+        res.send("{error}");
+    });
+});
+
+
+
+router.post("/user/add", (req, res) => {
+    res.send("OK");
+});
+
+
+
+
+
+
+
+
+router.use((req, res) => {
+    res.status(404);
+    res.json({
+        error: "API introuvable !"
+    });
+});
