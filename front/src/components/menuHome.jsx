2607e767de811df2d888cf8c708baadb7bb5fb36
Maxime-Thirion <thirionmaxime02@gmail.com>
2023-06-30 09:42:07 +0200

    cc

--- front/src/components/menuHome.jsx
+++ front/src/components/menuHome.jsx
@@ -8,8 +8,6 @@ const MenuPageHome = () => {
         <nav className="menu_home">
             <div className="links">
                 <ul>
-                    {/* <li><Link to="/Home#expl">Accueil</Link></li> */}
-                    {/* <li><a href="#expl" >Accueil</a></li> */}
                     <li><ScrollLink to="expl" smooth={true} duration={500} className="linkHome">Aide</ScrollLink></li>
                     <li><ScrollLink to="sign" smooth={true} duration={500} className="linkHome">Se connecter</ScrollLink></li>
                     <li><Link to="/Profil" className="linkHome">Mon Profil</Link></li>
