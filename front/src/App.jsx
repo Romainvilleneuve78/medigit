46aecca20607d2f560cd9956ee81ef6278b0a999
Maxime-Thirion <thirionmaxime02@gmail.com>
2023-06-30 09:27:08 +0200

    v1

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importez également Routes
-
-import Home from './Pages/Accueil/Accueil';
-import Page2 from './Pages/Ordonnances/Ordonnances';
-import Page3 from './Pages/Rappels/Rappels';
-import Profil from './Pages/Profil/Profil';
-import AddOrdo from './Pages/AddOrdo/AddOrdo';
-import Profil_pro from './Pages/Profil/Profil_pro';
-import Ordopdf from './Pages/Ordonnances/Ordopdf';
 import SignIn_Page from './Pages/SignIn_Page'
 import SignUp_Page from './Pages/SignUp_Page'
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importez également Routes
+
+import Page1 from './Pages/Accueil/Accueil';
+import Page2 from './Pages/Ordonnances/Ordonnances';
+import Page3 from './Pages/Rappels/Rappels';
+import Page4 from './Pages/Profil/Profil';
+import AddOrdo from './Pages/AddOrdo/AddOrdo';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,42 +21,43 @@ function App() {
   return (
     <>
       <Router>
-        <header>
-          <nav className="navbar">
-              <a href="/Home"><h1>MeDigit.</h1></a>
-              <h2>Recuperez vos ordonnaces n'importe où et nimporte quand</h2>
-              <a href="/SignIn_Page">
-                <div className="nav-links ">
-                    <ul>
-                        <li>Se connecter</li>
-                        <li><img className="user" src="images/user.png" alt="Logo"></img></li>
-                    </ul>
-                </div>
-              </a>
-          </nav>
-        </header>
+      <header>
+        <nav className="navbar">
+            <a href=""><h1>MeDigit.</h1></a>
+            <h2>Recuperez vos ordonnaces n'importe où et nimporte quand</h2>
+            <a href="">
+              <div className="nav-links ">
+                  <ul>
+                      <li>User123</li>
+                      <li><img className="user" src="images/user.png" alt="Logo"></img></li>
+                  </ul>
+              </div>
+            </a>
+        </nav>
+      </header>
 
-        
-      
-        <Routes> {/* Remplacez le composant Route par Routes */}
-          <Route path="/" element={<Home />} /> {/* Utilisez l'attribut element pour définir le composant */}
-          <Route path="/Home" element={<Home />} /> {/* Utilisez l'attribut element pour définir le composant */}
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/page3" element={<Page3 />} />
-          <Route path="/Profil" element={<Profil />} />
-          <Route path="/AddOrdo" element={<AddOrdo />} />
-          <Route path="/SignUp_Page" element={<SignUp_Page />} />
-          <Route path="/SignIn_Page" element={<SignIn_Page />} />
-          <Route path="/Profil_pro" element={<Profil_pro />} />
-          <Route path="/Ordopdf" element={<Ordopdf />} />
-        </Routes>
-
+      <nav className="menu">
+        <div className="links ">
+            <ul>
+              <li><Link to="/page1">Home</Link></li>
+              <li><Link to="/page2">Mes Ordonnances</Link></li>
+              <li><Link to="/page3">Mes Rappels</Link></li>
+              <li><Link to="/page4">Mon Profil</Link></li>
+              <li><Link to="/AddOrdo">Faire Ordonnance</Link></li>
+            </ul>
+        </div>    
+      </nav>
+    
+      <Routes> {/* Remplacez le composant Route par Routes */}
+        <Route path="/page1" element={<Page1 />} /> {/* Utilisez l'attribut element pour définir le composant */}
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+        <Route path="/AddOrdo" element={<AddOrdo />} />
+        <Route path="/SignUp_Page" element={<SignUp_Page />} />
+        <Route path="/SignIn_Page" element={<SignIn_Page />} />
+      </Routes>
       </Router>
-
-      <footer>
-          <h1>MeDigit.</h1>
-          <div class='txt'>Master Camp 2023</div>
-      </footer>
     </>
   )
 }
--- front/src/Pages/Accueil/Accueil.jsx
+++ front/src/Pages/Accueil/Accueil.jsx
@@ -1,71 +1,53 @@
 // src/pages/Page1.jsx
 import React from 'react';
 import './Accueil.css'
+import SignIn from '../../components/SignIn';
+import SignUp from '../../components/SignUp';
+import SignIn_Page from '../SignIn_Page'
+
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importez également Routes
-import MenuPageHome from '../../components/menuHome';
+import SignUp_Page from '../SignUp_Page';
+function Page1() {
 
-
-function Home() {
+  const slides = [
+    {
+      id: 1,
+      imageUrl: '../../../images/banniere1.jpeg',
+      title: 'Première image',
+      description: 'Description de la première image',
+    },
+    {
+      id: 2,
+      imageUrl: '../../../images/banniere2.jpeg',
+      title: 'Deuxième image',
+      description: 'Description de la deuxième image',
+    },
+    // Ajoutez plus de diapositives selon vos besoins
+  ];
 
   return (
     <>
-      <div class='header_extend'>
-        <div class='fond'>
+      <div class='Carousel'>
+        <div class='item'>
+          <div class='slide1'></div>
+        </div>
+      </div>
+      <h1>Accueil</h1>
+      <p>Mode d'emploi de l'utilisation du site web et comment obtenir une ordonnance</p>
 
-          <div class='txt'>
-            <div class='title'>
-              <h1>MeDigit</h1>
-              <h2>votre nouvelle plateforme de santé</h2>
-            </div>
-            <MenuPageHome/>
-          </div>
-          <img src='../../images/medecin.png'></img>
-        </div>
-        
-      </div>
-      
-    
-      <div class='help'>
-        <h1>Comment ca marche ?</h1>
-        <div class='division'>
-          <div class='picture'>
-            < img src="../../images/Ordonnance-electronique.png"></img>
-          </div>
+<nav>
 
-          <div class= 'explication'>
-            
-            <div class = 'steps'>
-              <div class='step'>1</div>
-              <div class='description'>Créez votre compte MeDigit.</div>
-            </div>
-            <div class = 'steps'>
-              <div class='step'>2</div>
-              <div class='description'>Enregistrez vos informations personnels.</div>
-            </div>
-            <div class = 'steps'>
-              <div class='step'>3</div>
-              <div class='description'>Accedez directement à vos ordonnances personnelles.</div>
-            </div>
-            <div class = 'steps'>
-              <div class='step'>4</div>
-              <div class='description'>Personnalisés vos rappels pour une vie plus tranquille.</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class='myspace'>
-        <div class='links'>
-          <h1>Votre espace Personnel</h1>
-          
-          <nav>
-            <div class='btn'><Link to="/SignIn_Page"><div class='a'>Vous connecter</div></Link></div>
-            <div class='btn'><Link to="/SignUp_Page"><div class='a'>Creer un compte</div></Link></div>
-          </nav>
-        </div>
-      </div>
+  <Link to="/SignIn_Page">Sign In</Link>
+  <Link to="/SignUp_Page">Sign Up</Link>
+</nav>
+<Routes> {/* Remplacez le composant Route par Routes */}
+  <Route path="/SignIn_Page" element={<SignIn_Page />} />
+  <Route path="/SignUp_Page" element={<SignUp_Page />} />
+  
+</Routes>
     </>
   );
 }
 
-export default Home;
+export default Page1;
