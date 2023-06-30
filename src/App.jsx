6696248a37b41df260a45d4f579e1364678bc447
Basile11 <basile.truquin@gmail.com>
2023-06-30 10:41:01 +0200

    merge franc

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SignIn_Page from './Pages/SignIn_Page'
 import SignUp_Page from './Pages/SignUp_Page'
