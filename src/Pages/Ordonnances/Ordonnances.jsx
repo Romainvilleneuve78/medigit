35aa1a54ebb438f02e629abc3526c24d99fb28bc
fdebo <francois.barbara.de.labelotterie.de.boisseson@efrei.net>
2023-07-04 10:29:39 +0200

    update

--- src/Pages/Ordonnances/Ordonnances.jsx
+++ src/Pages/Ordonnances/Ordonnances.jsx
@@ -77,7 +77,7 @@ function Ordonnance() {
                 <td>
                   <div className='container_ordo'>
                     <button className="pdp_ordo">
-                      <Link to="/Profil_pro">
+                      <Link to={`/Profil_pro/${user.user_id}`}>
                         <img src="../../images/pdp.png" alt="icone des points" />
                       </Link>
                     <span className="title_ordo">{user.FirstName} {user.LastName}</span></button>
