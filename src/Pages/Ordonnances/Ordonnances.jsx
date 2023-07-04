759a901e16634e9f1a75dbb1501ec1d993cedc9f
fdebo <francois.barbara.de.labelotterie.de.boisseson@efrei.net>
2023-07-04 19:03:29 +0200

    update

--- src/Pages/Ordonnances/Ordonnances.jsx
+++ src/Pages/Ordonnances/Ordonnances.jsx
@@ -36,21 +36,69 @@ function Ordonnance() {
           console.error('Une erreur s\'est produite lors de la récupération des prescriptions :', error);
         });
     }, []);
+    const handleFilterChange = event => {
+      setFilter(event.target.value);
+    };
+  
+    const filteredPrescriptions = prescriptions.filter(prescription =>
+      prescription.Name.toLowerCase().includes(filter.toLowerCase())
+    );
+  
+    return (
+      <>
+        <div className='header_ordo_extend'>
+          <div className='fond'>
+            <div className='txt'>
+              <MenuPage />
+              <h1>Mes ordonnances</h1>
+              <div className='btn_class'>
+                <input
+                  type="text"
+                  value={filter}
+                  onChange={handleFilterChange}
+                  placeholder="rechercher par symptome"
+                />
+              </div>
+            </div>
+            <img src='../../images/medecin3.png' alt="Image du médecin" />
+          </div>
+        </div>
+  
+        <div className='ordonnances'>
+          <table style={{ width: '100%' }}>
+            <tbody>
+              {filteredPrescriptions.map(prescription => (
+                <tr key={prescription.idPrescription}>
+                  <td>
+                    <div className='container_ordo'>
+                      <button className="pdp_ordo">
+                        <Link to={`/Profil_pro/${prescription.Professional}`}>
+                          <img src="../../images/pdp.png" alt="icone des points" />
+                        </Link>
+                      <span className="title_ordo">Voir le profil du médecin</span></button>
+  
+                      <div className='info_ordo'>
+                        <div className="Name_ordo">
+                          <h2><Link to={`/Ordopdf/${prescription.idPrescription}`}>{prescription.Name}</Link></h2>
+                          <p className="my-paragraph1">Valide jusqu'au {prescription.Date_validity}</p>
+                          <p className="my-paragraph2">Médicament prescrit: {prescription.Medicine}</p>
+                        </div>
+  
+                        <div className='option_ordo'>
+                          <button className="coeur"> <img src="../../images/coeur.png" alt="icone du bouton" /></button>
+                          <button className="poubelle"> <img src="../../images/poubelle-de-recyclage.png" alt="icone de poubelle" /></button>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
 
-    useEffect(() => {
-      if (prescriptions.length > 0) {
-        const idProfessional = prescriptions[0].Professional; // Utilisez le bon accès aux données pour obtenir l'ID du professionnel
-    
-        axios.get(`http://localhost:3000/professional/idprof/${idProfessional}`) // Utilisez l'URL avec idprof pour récupérer les informations de l'utilisateur
-          .then(response => {
-            setUser(response.data);
-            console.log(user);
-          })
-          .catch(error => {
-            console.error('Une erreur s\'est produite lors de la récupération des informations du professionnel :', error);
-          });
-      }
-    }, [prescriptions]);
 
   } else if (userKind === 1) {
     useEffect(() => {
@@ -65,21 +113,68 @@ function Ordonnance() {
           console.error('Une erreur s\'est produite lors de la récupération des prescriptions :', error);
         });
     }, []);
-
-    useEffect(() => {
-      if (prescriptions.length > 0) {
-        const idClient = prescriptions[0].Client; // Utilisez le bon accès aux données pour obtenir l'ID du professionnel
-    
-        axios.get(`http://localhost:3000/client/idclient/${idClient}`) // Utilisez l'URL avec idprof pour récupérer les informations de l'utilisateur
-          .then(response => {
-            setUser(response.data);
-            console.log(user);
-          })
-          .catch(error => {
-            console.error('Une erreur s\'est produite lors de la récupération des informations du professionnel :', error);
-          });
-      }
-    }, [prescriptions]);
+    const handleFilterChange = event => {
+      setFilter(event.target.value);
+    };
+  
+    const filteredPrescriptions = prescriptions.filter(prescription =>
+      prescription.Name.toLowerCase().includes(filter.toLowerCase())
+    );
+  
+    return (
+      <>
+        <div className='header_ordo_extend'>
+          <div className='fond'>
+            <div className='txt'>
+              <MenuPage />
+              <h1>Mes ordonnances</h1>
+              <div className='btn_class'>
+                <input
+                  type="text"
+                  value={filter}
+                  onChange={handleFilterChange}
+                  placeholder="rechercher par symptome"
+                />
+              </div>
+            </div>
+            <img src='../../images/medecin3.png' alt="Image du médecin" />
+          </div>
+        </div>
+  
+        <div className='ordonnances'>
+          <table style={{ width: '100%' }}>
+            <tbody>
+              {filteredPrescriptions.map(prescription => (
+                <tr key={prescription.idPrescription}>
+                  <td>
+                    <div className='container_ordo'>
+                      <button className="pdp_ordo">
+                        <Link to={`/Profil_pro/${prescription.Client}`}>
+                          <img src="../../images/pdp.png" alt="icone des points" />
+                        </Link>
+                      <span className="title_ordo">Voir le profil du client</span></button>
+  
+                      <div className='info_ordo'>
+                        <div className="Name_ordo">
+                          <h2><Link to={`/Ordopdf/${prescription.idPrescription}`}>{prescription.Name}</Link></h2>
+                          <p className="my-paragraph1">Valide jusqu'au {prescription.Date_validity}</p>
+                          <p className="my-paragraph2">Médicament prescrit: {prescription.Medicine}</p>
+                        </div>
+  
+                        <div className='option_ordo'>
+                          <button className="coeur"> <img src="../../images/coeur.png" alt="icone du bouton" /></button>
+                          <button className="poubelle"> <img src="../../images/poubelle-de-recyclage.png" alt="icone de poubelle" /></button>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
   };
 
 
@@ -90,68 +185,7 @@ function Ordonnance() {
 
   
 
-  const handleFilterChange = event => {
-    setFilter(event.target.value);
-  };
 
-  const filteredPrescriptions = prescriptions.filter(prescription =>
-    prescription.Name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  return (
-    <>
-      <div className='header_ordo_extend'>
-        <div className='fond'>
-          <div className='txt'>
-            <MenuPage />
-            <h1>Mes ordonnances</h1>
-            <div className='btn_class'>
-              <input
-                type="text"
-                value={filter}
-                onChange={handleFilterChange}
-                placeholder="rechercher par symptome"
-              />
-            </div>
-          </div>
-          <img src='../../images/medecin3.png' alt="Image du médecin" />
-        </div>
-      </div>
-
-      <div className='ordonnances'>
-        <table style={{ width: '100%' }}>
-          <tbody>
-            {filteredPrescriptions.map(prescription => (
-              <tr key={prescription.idPrescription}>
-                <td>
-                  <div className='container_ordo'>
-                    <button className="pdp_ordo">
-                      <Link to={`/Profil_pro/${user.user_id}`}>
-                        <img src="../../images/pdp.png" alt="icone des points" />
-                      </Link>
-                    <span className="title_ordo">{user.FirstName} {user.LastName}</span></button>
-
-                    <div className='info_ordo'>
-                      <div className="Name_ordo">
-                        <h2><Link to={`/Ordopdf/${prescription.idPrescription}`}>{prescription.Name}</Link></h2>
-                        <p className="my-paragraph1">Valide jusqu'au {prescription.Date_validity}</p>
-                        <p className="my-paragraph2">Médicament prescrit: {prescription.Medicine}</p>
-                      </div>
-
-                      <div className='option_ordo'>
-                        <button className="coeur"> <img src="../../images/coeur.png" alt="icone du bouton" /></button>
-                        <button className="poubelle"> <img src="../../images/poubelle-de-recyclage.png" alt="icone de poubelle" /></button>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
 }
 
 export default Ordonnance;
